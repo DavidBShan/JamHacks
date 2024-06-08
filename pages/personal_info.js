c059112f9129d9baca02412f31e072783dae70ed
punchdii <sunqiche@outlook.com>
2024-06-08 07:52:24 -0400

    langding page

--- pages/personal_info.js
+++ pages/personal_info.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from '../styles/personal_info.module.css';
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function Home() {
     const [journalEntries, setJournalEntries] = useState([
@@ -20,10 +21,17 @@ export default function Home() {
         }
     };
 
+    const { user, error, isLoading } = useUser();
+    const user_name = user ? user.name : "Guest"; // Fallback to "Guest" if user is not defined
+    const user_picture = user ? user.picture : "/default-profile.png"; // Fallback to a default profile picture
     return (
         <div className={styles.container}>
             <div className={styles.sidebar}>
-                <div className={styles.usernameSection}>Username</div>
+
+                <div className={styles.usernameSection}>
+                    {user_name}
+                    <img src={user_picture} alt="Profile" className={styles.profilePicture} />
+                </div>
                 <button className={styles.sidebarButton}>Connection</button>
                 <button className={styles.sidebarButton}>Personal Info</button>
                 <button className={styles.sidebarButton}>Journal</button>
